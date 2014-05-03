19ccdc2474396da1cdf8a4e153de08a3af0e1b7d
jinto1729 <jinto1729@gmail.com>
2014-05-03 07:09:43 +0530

    Sample first pages

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo '
+<html>
+Google Data Parser Project <br/>
+----------------------------------------------
+<form action="searchResults.php" name="searchForm" method="GET">
+
+<input name="query1" style="width: 435px; height: 33px" type="text"/>
+<br/>
+<input name="query2" style="width: 435px; height: 33px" type="text"/>
+<br/>
+<input name="search" id="search" style="width: 108px; height: 30px" type="submit" value="Search"/>
+</form>
+</html>
+';
+//----
+?>
